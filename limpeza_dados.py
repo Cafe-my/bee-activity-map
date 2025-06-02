e8bce231c5cf085410d800c1a3ea4e49502827ac
Michele Ye <michele.ye.dev@gmail.com>
2025-06-02 18:36:30 -0300

    subindo o arquivo de limpeza de dados

--- limpeza_dados.py
+++ limpeza_dados.py
@@ -1,13 +1,28 @@
 import pandas as pd
 
-df = pd.read_csv('./dados_abelha.csv')
+def limpar_dados(path_csv):
+    df = pd.read_csv(path_csv)
 
+# Remove as colunas 100% vazias
+    df = df.dropna(axis=1, how='all')
 
+# Troca os valores 'Rs' na coluna Estado por 'Rio Grande do Sul'
+    valores_Rs = df['Estado'] == 'Rs'
+    df[valores_Rs]['Estado'] = 'Rio Grande do Sul'
 
-# Corrigir a escrita da data de registro e modificar o dtype para datetime
-# Eliminar linha com elementos nulos (2 no Estado)
-#Eliminar 1 registro com Estado == Misiones
-# trocar os valores 'Rs' na coluna Estado por 'Rio Grande do Sul'
-# apagar coluna vazia ID de ocorrencia
-#UTILIZAR O ARQUIVO 2 DDE DADOS ABELHA
-print(df.info())
+# Elimina linhas com valores 'Misiones' na coluna 'Estado'
+    df = df[df['Estado'] != 'Misiones']
+
+# Descartar as horas e modificar o dtype para datetime
+    df['Data do Registro'] = pd.to_datetime(df['Data do Registro'], errors='coerce', format='ISO8601').dt.date
+    df['Data do Registro'] = pd.to_datetime(df['Data do Registro'], errors='coerce')
+
+# Remove todos as linhas com valores NaN
+    df = df.dropna()
+
+# Salva em um novo arquivo csv
+    df.to_csv('dados_abelha_limpos.csv', index=False)
+    print('Dados salvos com sucesso em: dados_abelha_limpos.csv')
+
+if __name__ == '__main__':
+    limpar_dados('./dados_abelha.csv')
--- taxon.py
+++
@@ -1,4 +0,0 @@
-taxon_keys = [1334757, 1340042, 1, 1342048, 1340278, 1340556]       #["Apis", "Melipona", "Trigona", "Xylocopa", "Bombus", "Euglossa"]
-
-params = [("taxonKey", str(key)) for key in taxon_keys]
-print(params)
